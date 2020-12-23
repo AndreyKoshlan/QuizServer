ac27da599b176e9a5882b37d4767bfd47d877c60
AndreyKoshlan <minebobrb@gmail.com>
2020-12-23 21:57:46 +0300

    Finish test feature

--- api/test.php
+++ api/test.php
@@ -29,20 +29,61 @@ function StartTest($conn, $testid, $sid) {
 		return json_encode($ret);
 	}
 	
-function CreateTest($conn, $sid, $content, $answers, $name, $groupid, $uid){
+function CreateTest($conn, $sid, $content, $answers, $name, $groupid, $uid) {
 		if (CheckSid($conn, $sid)){
 			$sql = "INSERT INTO tests (content, answers, name, groupid, uid) VALUES ('".$content."', '".$answers."', '".$name."', '".$groupid."', '".$uid."')";
 			if ($conn->query($sql) === TRUE) {
 				$ret->status = 1;
 			} else {
 				$ret->status = 0;
-				$ret->error = "Test not created";
+				$ret->error = "Can''t create test";
 			}
 		} else {
 			$ret->status = 0;
 			$ret->error = "Token is incorrect";
 		}
 	}
+
+function CompareAnswers($client, $server) {
+	$jc = json_decode($client, true);
+	$js = json_decode($server, true);
+	$points = 0;
+	$questions = 0;
+	for ($i = 0; $i < count($js["pages"]); $i++) {
+		$questions++;
+		if ($jc["pages"] === $js["pages"]) {
+			$points++;
+		}
+	}
+	$ret->status = 1;
+	$ret->result->correct = $points;
+	$ret->result->count = $questions;
+	if ($questions === 0) {
+		$ret->result->ratio = 1;
+	} else {
+		$ret->result->ratio = $points/$questions;
+	}
+	$ret->result->percent = strval($ret->result->ratio*100)."%";
+	return json_encode($ret);
+}
+
+function FinishTest($conn, $sid, $testid, $answers) {
+	if (CheckSid($conn, $sid)) {
+		$sql = "SELECT answers FROM tests WHERE testid = '".$testid."'";
+		$rez = $conn->query($sql);
+		if ($rez->num_rows > 0) {
+			$row = $rez->fetch_assoc();
+			return CompareAnswers($answers, $row["answers"]);
+		} else {
+			$ret->status = 0;
+			$ret->error = "Test not found";
+		}
+	} else {
+		$ret->status = 0;
+		$ret->error = "Token is incorrect";
+	}
+	return json_encode($ret);
+}
 
 require 'connect.php';
 	$msgtype = filter_var(trim($_POST['type']), FILTER_SANITIZE_STRING);
@@ -62,7 +103,7 @@ require 'connect.php';
 		$sid = filter_var(trim($_POST['sid']), FILTER_SANITIZE_STRING);
 		$testid = filter_var(trim($_POST['testid']), FILTER_SANITIZE_STRING);
 		$answers = filter_var(trim($_POST['answers']), FILTER_SANITIZE_STRING);
-		echo getUserID($conn, $sid);
+		echo FinishTest($conn, $sid, $testid, $answers);
 	}
 	if ($msgtype === "create") {
 		$sid = filter_var(trim($_POST['sid']), FILTER_SANITIZE_STRING);
